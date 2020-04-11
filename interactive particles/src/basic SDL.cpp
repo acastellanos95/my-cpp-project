6557f8bbdd8b9f72009e029c36244a5bf06cadce
acastellanos95 <acastellanosaldama@gmail.com>
2020-04-10 23:53:47 -0500

    change pixels color

--- interactive particles/src/basic SDL.cpp
+++ interactive particles/src/basic SDL.cpp
@@ -25,6 +25,33 @@ int main() {
 		return 1;
 	}
 
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	if(renderer == nullptr){
+		cout << "Could not create renderer" << "\n";
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 2;
+	}
+	if(texture == nullptr){
+		cout << "Could not create texture" << "\n";
+		SDL_DestroyWindow(window);
+		SDL_DestroyTexture(texture);
+		SDL_Quit();
+		return 3;
+	}
+
+	Uint32* buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
+
+	memset(buffer, 255, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+
+	SDL_UpdateTexture(texture, nullptr, buffer, SCREEN_WIDTH*sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+	SDL_RenderPresent(renderer);
+
 	bool quit = false;
 
 	SDL_Event event;
@@ -42,7 +69,9 @@ int main() {
 
 	}
 
-	cout << "SDL Init success";
+	delete [] buffer;
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(texture);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
